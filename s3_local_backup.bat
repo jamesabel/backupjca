983bbdebd0528a0e6941345b4418a2f42add67da
james abel <j@abel.co>
2019-06-15 22:20:23 -0700

    proper logging

---
+++ s3_local_backup.bat
@@ -0,0 +1,3 @@
+venv\Scripts\activate
+python s3_local_backup.py temp
+deactivate
--- s3_local_backup/s3_local_backup.py
+++ s3_local_backup/s3_local_backup.py
@@ -6,7 +6,11 @@ import os
 import re
 import math
 
-from s3_local_backup import __version__, __description__
+from balsa import Balsa, get_logger
+
+from s3_local_backup import __application_name__, __author__, __version__, __description__
+
+log = get_logger(__application_name__)
 
 
 # sundry candidate
@@ -22,43 +26,56 @@ def get_dir_size(dir_path):
 
 def s3_local_backup(backup_directory: str, aws_profile: str, dry_run: bool):
 
-    with open('s3_local_backup.log', 'w') as log_file:
+    os.makedirs(backup_directory, exist_ok=True)
+    log_file_path = os.path.join(backup_directory, )
 
-        if aws_profile is not None:
-            session = boto3.Session(profile_name=aws_profile)
-            s3_client = session.client('s3')
+    log.info(f"{__application_name__} : {__version__}")
+
+    if aws_profile is not None:
+        session = boto3.Session(profile_name=aws_profile)
+        s3_client = session.client('s3')
+    else:
+        s3_client = boto3.client('s3')
+
+    decoding = "utf-8"
+
+    # we delete all whitespace below
+    ls_re = re.compile(r"TotalObjects:([0-9]+)TotalSize:([0-9]+)")
+
+    buckets = s3_client.list_buckets()['Buckets']
+    s = f"found {len(buckets)} buckets"
+    log.info(s)
+    print(s)
+
+    for bucket in buckets:
+        bucket_name = bucket['Name']
+
+        # do the sync
+        log.info(f"bucket : {bucket_name}")
+        destination = os.path.join(backup_directory, bucket_name)
+        os.makedirs(destination, exist_ok=True)
+        s3_bucket_path = f"s3://{bucket_name}"
+        sync_command_line = ['aws', 's3', 'sync', s3_bucket_path, destination]
+        if dry_run:
+            sync_command_line.append('--dryrun')
+        log.info(str(sync_command_line))
+        sync_result = subprocess.run(sync_command_line, stdout=subprocess.PIPE)
+        for line in sync_result.stdout.decode(decoding).splitlines():
+            log.info(line.strip())
+
+        # check the results
+        ls_command_line = ['aws', 's3', 'ls', '--summarize', '--recursive', s3_bucket_path]
+        log.info(str(ls_command_line))
+        ls_result = subprocess.run(ls_command_line, stdout=subprocess.PIPE)
+        ls_stdout = ''.join([c for c in ls_result.stdout.decode(decoding) if c not in ' \r\n'])  # remove all whitespace
+        ls_parsed = ls_re.search(ls_stdout)
+        s3_object_count = int(ls_parsed.group(1))
+        s3_total_size = int(ls_parsed.group(2))
+        local_size, local_count = get_dir_size(destination)
+        if not math.isclose(s3_total_size, local_size, rel_tol=0.01) or not math.isclose(s3_object_count, local_count, rel_tol=0.01):
+            log.error(f"Error: {bucket_name} not properly backed up (s3_count={s3_object_count}, local_count={local_count}; s3_total_size={s3_total_size}, local_size={local_size})")
         else:
-            s3_client = boto3.client('s3')
-
-        decoding = "utf-8"
-
-        # we delete all whitespace below
-        ls_re = re.compile(r"TotalObjects:([0-9]+)TotalSize:([0-9]+)")
-
-        buckets = s3_client.list_buckets()['Buckets']
-        print(f"found {len(buckets)} buckets")
-        for bucket in buckets:
-            bucket_name = bucket['Name']
-
-            destination = os.path.join(backup_directory, bucket_name)
-            os.makedirs(destination, exist_ok=True)
-            s3_bucket_path = f"s3://{bucket_name}"
-            sync_command_line = ['aws', 's3', 'sync', s3_bucket_path, destination]
-            if dry_run:
-                sync_command_line.append('--dryrun')
-            sync_result = subprocess.run(sync_command_line, stdout=subprocess.PIPE)
-            log_file.write(sync_result.stdout.decode(decoding))
-
-            ls_command_line = ['aws', 's3', 'ls', '--summarize', '--recursive', s3_bucket_path]
-            ls_result = subprocess.run(ls_command_line, stdout=subprocess.PIPE)
-            ls_stdout = ''.join([c for c in ls_result.stdout.decode(decoding) if c not in ' \r\n'])  # remove all whitespace
-            ls_parsed = ls_re.search(ls_stdout)
-            s3_object_count = int(ls_parsed.group(1))
-            s3_total_size = int(ls_parsed.group(2))
-
-            local_size, local_count = get_dir_size(destination)
-            if not math.isclose(s3_total_size, local_size, rel_tol=0.01) or not math.isclose(s3_object_count, local_count, rel_tol=0.01):
-                print(f"Error: {bucket_name} not properly backed up (s3_count={s3_object_count}, local_count={local_count}; s3_total_size={s3_total_size}, local_size={local_size})")
+            log.info(f"{bucket_name} : s3_count={s3_object_count}, s3_total_size={s3_total_size}")
 
 
 def main():
@@ -71,5 +88,11 @@ def main():
     parser.add_argument('-d', '--dry_run', action='store_true', default=False,
                         help="Displays the operations that would be performed using the specified command without actually running them")
     args = parser.parse_args()
+
+    balsa = Balsa(__application_name__, __author__)
+    balsa.verbose = True
+    balsa.log_directory = args.path
+    balsa.delete_existing_log_files = True
+    balsa.init_logger()
 
     s3_local_backup(args.path, args.profile, args.dry_run)
