3e212fcf2fdd765b3bed851cbe4fe31b058f19bb
james abel <j@abel.co>
2019-09-04 22:28:23 -0700

    take compare_dirs out and move it to fsu project/package

--- backupjca/__init__.py
+++ backupjca/__init__.py
@@ -1,6 +1,5 @@
 
 from .__version__ import __application_name__, __version__, __description__, __author__, __python_version__
-from .compare_dirs import compare_dirs, compare_dirs_main, windows_long_path_prefix, ignore_files
 from .s3_local_backup import s3_local_backup
 from .git_local_backup import git_local_backup
 from ._backupjca import main
--- test/test_compare_dirs_main.py
+++
@@ -1,8 +0,0 @@
-
-from backupjca import main
-
-
-def test_compare_dirs_main():
-    # todo: need to pass in the 2 paths (else pytest fails)
-    # main()
-    pass
